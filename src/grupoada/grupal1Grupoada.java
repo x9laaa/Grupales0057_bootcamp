54e7d3d8fcb55cb42c18a3966046d7bd5675b1ce
Pablo Aliaga <pandresaliaga@gmail.com>
2023-06-12 22:11:35 -0400

    arreglo archivo dos

--- src/grupoada/grupal1Grupoada.java
+++ src/grupoada/grupal1Grupoada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AccidenteACH {
+public class grupal1Grupoada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nombres, apellidos, cargo;
