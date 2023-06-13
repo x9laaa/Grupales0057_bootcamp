e82cf7e22c4fd2065a9c4192a37195c3c71a18d2
fvmolina21 <fvmolina@uc.cl>
2023-06-12 21:10:55 -0400

    arreglo condoro

--- src/bytemaster/java/Grupal4.java
+++ src/bytemaster/java/Grupal4.java
@@ -21,58 +21,51 @@ public class Grupal4 {
         System.out.print("Ingrese el tipo de perfil (cliente, profesional, administrativo): ");
         String tipoPerfil = sc.nextLine();
 
-        switch (tipoPerfil.toLowerCase()) {
-            case "cliente":
-                System.out.print("Ingrese la dirección: ");
-                String direccion = sc.nextLine();
-                System.out.print("Ingrese el teléfono: ");
-                String telefono = sc.nextLine();
-                System.out.print("Ingrese la cantidad de empleados: ");
-                int cantidadEmpleados = sc.nextInt();
-                sc.nextLine();
-
-                System.out.println("\nDatos del Cliente #" + i);
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
-                System.out.println("RUN: " + run);
-                System.out.println("Dirección: " + direccion);
-                System.out.println("Teléfono: " + telefono);
-                System.out.println("Cantidad de Empleados: " + cantidadEmpleados);
-                break;
-
-            case "profesional":
-                System.out.print("Ingrese los años de experiencia: ");
-                int aniosExperiencia = sc.nextInt();
-                sc.nextLine();
-                System.out.print("Ingrese el departamento: ");
-                String departamento = sc.nextLine();
-
-                System.out.println("\nDatos del Profesional #" + i);
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
-                System.out.println("RUN: " + run);
-                System.out.println("Años de Experiencia: " + aniosExperiencia);
-                System.out.println("Departamento: " + departamento);
-                break;
-
-            case "administrativo":
-                System.out.print("Ingrese la función: ");
-                String funcion = sc.nextLine();
-                System.out.print("Ingrese el nombre del superior: ");
-                String nombreSuperior = sc.nextLine();
-
-                System.out.println("\nDatos del Administrativo #" + i);
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
-                System.out.println("RUN: " + run);
-                System.out.println("Función: " + funcion);
-                System.out.println("Nombre del Superior: " + nombreSuperior);
-                break;
-
-            default:
-                System.out.println("Tipo de perfil inválido.");
-                break;
-        }
+            switch (tipoPerfil.toLowerCase()) {
+                
+                case "cliente" -> {
+                    System.out.print("Ingrese la dirección: ");
+                    String direccion = sc.nextLine();
+                    System.out.print("Ingrese el teléfono: ");
+                    String telefono = sc.nextLine();
+                    System.out.print("Ingrese la cantidad de empleados: ");
+                    int cantidadEmpleados = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("\nDatos del Cliente #" + i);
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
+                    System.out.println("RUN: " + run);
+                    System.out.println("Dirección: " + direccion);
+                    System.out.println("Teléfono: " + telefono);
+                    System.out.println("Cantidad de Empleados: " + cantidadEmpleados);
+                }
+                case "profesional" -> {
+                    System.out.print("Ingrese los años de experiencia: ");
+                    int aniosExperiencia = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Ingrese el departamento: ");
+                    String departamento = sc.nextLine();
+                    System.out.println("\nDatos del Profesional #" + i);
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
+                    System.out.println("RUN: " + run);
+                    System.out.println("Años de Experiencia: " + aniosExperiencia);
+                    System.out.println("Departamento: " + departamento);
+                }
+                case "administrativo" -> {
+                    System.out.print("Ingrese la función: ");
+                    String funcion = sc.nextLine();
+                    System.out.print("Ingrese el nombre del superior: ");
+                    String nombreSuperior = sc.nextLine();
+                    System.out.println("\nDatos del Administrativo #" + i);
+                    System.out.println("Nombre: " + nombre);
+                    System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
+                    System.out.println("RUN: " + run);
+                    System.out.println("Función: " + funcion);
+                    System.out.println("Nombre del Superior: " + nombreSuperior);
+                }
+                default -> System.out.println("Tipo de perfil inválido.");
+            }
 
         System.out.println();
     }
