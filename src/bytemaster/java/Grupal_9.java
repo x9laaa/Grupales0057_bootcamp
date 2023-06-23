3761ddee918f24f7fb258d80c2e453576eaffa1f
fvmolina21 <fvmolina@uc.cl>
2023-06-22 21:54:18 -0400

    Grupal_9

---
+++ src/bytemaster/java/Grupal_9.java
@@ -0,0 +1,22 @@
+package bytemaster.java;
+
+/*Una empresa de asesorías en prevención de riesgos necesita contar con un sistema de información
+que le permita administrar los principales procesos que se llevan a cabo en ella día a día.
+Dentro de las actividades que realiza, están las capacitaciones. Estas son instancias que se
+desarrollan para todos los clientes que posee (que corresponden generalmente a otras empresas).
+Por cada capacitación se desea tener registro del RUT del cliente, su nombre, dirección, comuna y
+número de teléfono. En cuanto a la capacitación misma, se necesita saber el día, hora, lugar,
+duración y cantidad de asistentes.
+De acuerdo con la cantidad de asistentes anteriormente ingresada, se debe solicitar y el nombre y
+edad de los asistentes. Finalizado este proceso, debe mostrar por pantalla los datos de la empresa
+que pidió la capacitación, los datos de la capacitación misma, y la cantidad de personas menores a
+25 años, entre 26 y 35 años y mayores a 35 años.
+Nota: Todos los datos son obligatorios. Por tanto, debe validar que el ingreso haya sido correcto en
+todos los casos. Además, debe validar que la cantidad de asistentes registrada sea mayor que cero*/
+
+public class Grupal_9 {
+
+
+
+
+}
