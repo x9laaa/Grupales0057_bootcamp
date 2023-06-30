9a71fde5647ca47ac51093735aec115a98a92a0b
x9laaa <acarreno95@outlook.com>
2023-06-30 19:32:39 -0400

    prueba de credencial 3

--- src/bytemaster/java/grupal12/dia8/Grupal_8V2.java
+++ src/bytemaster/java/grupal12/dia8/Grupal_8V2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * Clase que registra los datos de diferentes tipos de usuarios.
  * @author Bytemaster
- * @version 1.2
+ * @version 1.3
  */
 
 public class Grupal_8V2 {
