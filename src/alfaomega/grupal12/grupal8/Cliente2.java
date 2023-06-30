f502ae4cd70a8b753e6df555f78f6f3a23579357
Lushiitooh <luis.sepu.c@gmail.com>
2023-06-29 21:58:11 -0400

    se realiza javadoc a los ejercicios grupales 8, 9, 10 del Ejercicio Grupal 12

--- src/alfaomega/grupal12/grupal8/Cliente2.java
+++ src/alfaomega/grupal12/grupal8/Cliente2.java
@@ -1,5 +1,12 @@
 package alfaomega.grupal12.grupal8;
 
+/**
+ *
+ *  Clase que representa a un cliente
+ *
+ * @author AlfaOmega
+ * @version 1.0
+ */
 public class Cliente2 {
         private String nombre;
         private String fechaNacimiento;
@@ -8,6 +15,15 @@ public class Cliente2 {
         private String telefono;
         private int cantidadEmpleados;
 
+    /**
+     *
+     * @param nombre el nombre del cliente
+     * @param fechaNacimiento la fecha de nacimiento del cliente
+     * @param run el RUN del cliente
+     * @param direccion La direccion del cliente
+     * @param telefono el telefono del cliente
+     * @param cantidadEmpleados La cantidad de empleados del Cliente
+     */
         // Constructor
         public Cliente2(String nombre, String fechaNacimiento, String run, String direccion, String telefono, int cantidadEmpleados) {
             this.nombre = nombre;
@@ -17,52 +33,102 @@ public class Cliente2 {
             this.telefono = telefono;
             this.cantidadEmpleados = cantidadEmpleados;
         }
-
+    /**
+     * Obtiene el nombre del cliente.
+     *
+     * @return El nombre del cliente.
+     */
         //  atributos
         public String getNombre() {
             return nombre;
         }
+    /**
+     * Establece el nombre del cliente.
+     *
+     * @param nombre El nombre del cliente.
+     */
 
         public void setNombre(String nombre) {
             this.nombre = nombre;
         }
-
+    /**
+     * Obtiene la fecha de nacimiento del cliente.
+     *
+     * @return La fecha de nacimiento del cliente.
+     */
         public String getFechaNacimiento() {
             return fechaNacimiento;
         }
-
+    /**
+     * Establece la fecha de nacimiento del cliente.
+     *
+     * @param fechaNacimiento La fecha de nacimiento del cliente.
+     */
         public void setFechaNacimiento(String fechaNacimiento) {
             this.fechaNacimiento = fechaNacimiento;
         }
-
+    /**
+     * Obtiene el RUN del cliente.
+     *
+     * @return El RUN del cliente.
+     */
         public String getRun() {
             return run;
         }
-
+    /**
+     * Establece el RUN del cliente.
+     *
+     * @param run El RUN del cliente.
+     */
         public void setRun(String run) {
             this.run = run;
         }
 
+    /**
+     * Obtiene la dirección del cliente.
+     *
+     * @return La dirección del cliente.
+     */
         public String getDireccion() {
             return direccion;
         }
-
+    /**
+     * Establece la dirección del cliente.
+     *
+     * @param direccion La dirección del cliente.
+     */
         public void setDireccion(String direccion) {
             this.direccion = direccion;
         }
-
+    /**
+     * Obtiene el teléfono del cliente.
+     *
+     * @return El teléfono del cliente.
+     */
         public String getTelefono() {
             return telefono;
         }
-
+    /**
+     * Establece el teléfono del cliente.
+     *
+     * @param telefono El teléfono del cliente.
+     */
         public void setTelefono(String telefono) {
             this.telefono = telefono;
         }
-
+    /**
+     * Obtiene la cantidad de empleados del cliente.
+     *
+     * @return La cantidad de empleados del cliente.
+     */
         public int getCantidadEmpleados() {
             return cantidadEmpleados;
         }
-
+    /**
+     * Establece la cantidad de empleados del cliente.
+     *
+     * @param cantidadEmpleados La cantidad de empleados del cliente.
+     */
         public void setCantidadEmpleados(int cantidadEmpleados) {
             this.cantidadEmpleados = cantidadEmpleados;
         }
--- src/alfaomega/grupal12/grupal9/grupal_9_2.java
+++ src/alfaomega/grupal12/grupal9/grupal_9_2.java
@@ -1,8 +1,16 @@
 package alfaomega.grupal12.grupal9;
 
 import java.util.*;
+/**
+ * Esta clase representa un programa para gestionar capacitaciones y consultas de capacitaciones.
+ * @author AlfaOmega
+ * @version 1.0
+ */
 public class grupal_9_2 {
-
+    /**
+     * El punto de entrada del programa.
+     * @param args Los argumentos de línea de comandos.
+     */
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             Map<String, Capacitacion2> capacitaciones = new HashMap<>();
@@ -36,7 +44,11 @@ public class grupal_9_2 {
             scanner.close();
             System.out.println("¡Hasta luego!");
         }
-
+    /**
+     * Permite ingresar una nueva capacitación y almacenarla en el mapa de capacitaciones.
+     * @param scanner El objeto Scanner utilizado para recibir la entrada del usuario.
+     * @param capacitaciones El mapa de capacitaciones donde se almacenará la nueva capacitación.
+     */
         private static void ingresarCapacitacion(Scanner scanner, Map<String, Capacitacion2> capacitaciones) {
             System.out.println("Ingrese el RUT del cliente:");
             String rut = scanner.nextLine();
@@ -91,6 +103,11 @@ public class grupal_9_2 {
             System.out.println("Capacitación ingresada exitosamente.");
         }
 
+    /**
+     * Permite consultar una capacitación por el RUT del cliente y muestra los datos de la capacitación.
+     * @param scanner El objeto Scanner utilizado para recibir la entrada del usuario.
+     * @param capacitaciones El mapa de capacitaciones donde se buscará la capacitación.
+     */
         private static void consultarCapacitacion(Scanner scanner, Map<String, Capacitacion2> capacitaciones) {
             System.out.println("Ingrese el RUT del cliente:");
             String rut = scanner.nextLine();
@@ -137,6 +154,9 @@ public class grupal_9_2 {
 
     }
 
+    /**
+    * Esta clase representa una capacitación.
+    */
     class Capacitacion2 {
         private String rut;
         private String nombreCliente;
@@ -150,6 +170,20 @@ public class grupal_9_2 {
         private List<String> nombresAsistentes;
         private List<Integer> edadesAsistentes;
 
+        /**
+         * Crea una nueva instancia de la clase Capacitacion2.
+         * @param rut El RUT del cliente.
+         * @param nombreCliente El nombre del cliente.
+         * @param direccion La dirección del cliente.
+         * @param comuna La comuna del cliente.
+         * @param telefono El número de teléfono del cliente.
+         * @param dia El día de la capacitación.
+         * @param hora La hora de la capacitación.
+         * @param lugar El lugar de la capacitación.
+         * @param duracion La duración de la capacitación en horas.
+         * @param nombresAsistentes Los nombres de los asistentes.
+         * @param edadesAsistentes Las edades de los asistentes.
+         */
         public Capacitacion2(String rut, String nombreCliente, String direccion, String comuna, String telefono,
                              String dia, String hora, String lugar, int duracion, List<String> nombresAsistentes,
                              List<Integer> edadesAsistentes) {
@@ -165,47 +199,80 @@ public class grupal_9_2 {
             this.nombresAsistentes = nombresAsistentes;
             this.edadesAsistentes = edadesAsistentes;
         }
-
+        /**
+         * Devuelve el RUT del cliente.
+         * @return El RUT del cliente.
+         */
         public String getRut() {
             return rut;
         }
-
+        /**
+         * Devuelve el nombre del cliente.
+         * @return El nombre del cliente.
+         */
         public String getNombreCliente() {
             return nombreCliente;
         }
-
+        /**
+         * Devuelve la dirección del cliente.
+         * @return La dirección del cliente.
+         */
         public String getDireccion() {
             return direccion;
         }
-
+        /**
+         * Devuelve la comuna del cliente.
+         * @return La comuna del cliente.
+         */
         public String getComuna() {
             return comuna;
         }
-
+        /**
+         * Devuelve el número de teléfono del cliente.
+         * @return El número de teléfono del cliente.
+         */
         public String getTelefono() {
             return telefono;
         }
-
+        /**
+         * Devuelve el día de la capacitación.
+         * @return El día de la capacitación.
+         */
         public String getDia() {
             return dia;
         }
-
+        /**
+         * Devuelve la hora de la capacitación.
+         * @return La hora de la capacitación.
+         */
         public String getHora() {
             return hora;
         }
-
+        /**
+         * Devuelve el lugar de la capacitación.
+         * @return El lugar de la capacitación.
+         */
         public String getLugar() {
             return lugar;
         }
-
+        /**
+         * Devuelve la duración de la capacitación en horas.
+         * @return La duración de la capacitación en horas.
+         */
         public int getDuracion() {
             return duracion;
         }
-
+        /**
+         * Devuelve los nombres de los asistentes.
+         * @return Los nombres de los asistentes.
+         */
         public List<String> getNombresAsistentes() {
             return nombresAsistentes;
         }
-
+        /**
+         * Devuelve las edades de los asistentes.
+         * @return Las edades de los asistentes.
+         */
         public List<Integer> getEdadesAsistentes() {
             return edadesAsistentes;
         }
