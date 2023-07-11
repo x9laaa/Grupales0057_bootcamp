87687b9a403402dbc77d8bcc01ceec0833894f89
cpmeneses <cpmeneses94@gmail.com>
2023-07-11 19:36:46 -0400

    grupal 14, Revision, Gruapl 16, Cliente

--- src/codingclub404/modulo04/grupal14/Revision.java
+++ src/codingclub404/modulo04/grupal14/Revision.java
@@ -1,2 +1,8 @@
-package codingclub404.modulo04.grupal14;public class Revision {
+package codingclub404.modulo04.grupal14;
+
+public class Revision {
+    private int idRevision;
+    private String nombreRevision;
+    private String detalleParaReviar;
+    private int estado;
 }
---
+++ src/codingclub404/modulo04/grupal16/Cliente.java
@@ -0,0 +1,76 @@
+package codingclub404.modulo04.grupal16;
+
+public class Cliente {
+    private int rut;
+    private String razonSocial;
+    private String telefono;
+    private String direccion;
+    private String comuna;
+
+    public Cliente() {
+    }
+
+    public Cliente(int rut, String razonSocial, String telefono, String direccion, String comuna) {
+        this.rut = rut;
+        this.razonSocial = razonSocial;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.comuna = comuna;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "rut=" + rut +
+                ", razonSocial='" + razonSocial + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", comuna='" + comuna + '\'' +
+                '}';
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+
+    public String obtenerNombre() {
+        return this.razonSocial + " " + this.rut;
+    }
+
+}
