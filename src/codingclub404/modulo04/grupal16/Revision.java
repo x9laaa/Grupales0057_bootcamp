515ce9621e9acabece33423dd1e9b7739ba6b45c
cpmeneses <cpmeneses94@gmail.com>
2023-07-11 19:40:28 -0400

    modulo 4, grupal16, clases

---
+++ src/codingclub404/modulo04/grupal16/Revision.java
@@ -0,0 +1,8 @@
+package codingclub404.modulo04.grupal16;
+
+public class Revision {
+    private int idRevision;
+    private String nombreRevision;
+    private String detalleParaReviar;
+    private int estado;
+}
